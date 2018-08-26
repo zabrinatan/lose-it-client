7c386c18359bce8a22be78be07e840f1c8fd185b
Zabrina L <zabrinatan@hotmail.com>
2018-08-26 15:28:00 +1000

    myrecipes component added

---
+++ src/components/MyRecipes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class MyRecipes extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default MyRecipes
