8ddc203611696b4f15b8254e48a9e046720f4604
Zabrina L <zabrinatan@hotmail.com>
2018-08-27 16:47:04 +1000

    created get request for all mealplans for users

--- src/components/MyRecipes.js
+++ src/components/MyRecipes.js
@@ -6,6 +6,7 @@ import Header from './Header'
 
 const SERVER_MEALPLAN = "http://localhost:3000/mealplans/addmeal"
 const SERVER_MY_RECIPES = "http://localhost:3000/recipes.json"
+const SERVER_MY_MEALPLANS = "http://localhost:3000/mealplans.json"
 class MyRecipes extends Component {
   constructor(){
     super();
@@ -13,7 +14,12 @@ class MyRecipes extends Component {
       favourites: [],
       day: "",
       meal: "",
-      recipe: ""
+      recipe: "",
+      SundayBreakfast: "",
+      MondayBreakfast: "",
+      TuesdayBreakfast: "",
+      WednesdayBreakfast: "",
+      ThursdayBreakfast: ""
     }
     this._onSubmit = this._onSubmit.bind(this)
 
@@ -21,6 +27,7 @@ class MyRecipes extends Component {
 componentDidMount(){
 
   this.fetchRecipes();
+  this.fetchMealPlans();
 }
   fetchRecipes(){
     axios.get(SERVER_MY_RECIPES, {headers: {
@@ -33,6 +40,15 @@ componentDidMount(){
     })
   }
 
+  fetchMealPlans(){
+    axios.get(SERVER_MY_MEALPLANS, {headers: {
+             "Authorization": localStorage.getItem('jwt')
+           }
+         }).then((response)=> {
+           console.log(response)
+         })
+  }
+
 _onSubmit(e){
   e.preventDefault();
   const recipe = document.getElementById(e.target.id);
@@ -42,7 +58,6 @@ _onSubmit(e){
 }
 
 _onClick(day, meal, e){
-  console.log(day)
   this.setState({
     meal: meal,
     day: day
@@ -100,35 +115,35 @@ _handleClick(label, e){
         <tbody>
           <tr>
             <td>Breakfast</td>
-            <td id = "1" onClick = {this._onClick.bind(this, 'Sunday', 'Breakfast')}></td>
-            <td id = "2" onClick = {this._onClick.bind(this, 'Monday', 'Breakfast')}></td>
-            <td id = "3" onClick = {this._onClick.bind(this, 'Tuesday', 'Breakfast')}></td>
-            <td id = "4" onClick = {this._onClick.bind(this, 'Wednesday', 'Breakfast')}></td>
-            <td id = "5" onClick = {this._onClick.bind(this, 'Thursday', 'Breakfast')}></td>
-            <td id = "6" onClick = {this._onClick.bind(this, 'Friday', 'Breakfast')}></td>
+            <td id = "SundayBreakfast" onClick = {this._onClick.bind(this, 'Sunday', 'Breakfast')}></td>
+            <td id = "MondayBreakfast" onClick = {this._onClick.bind(this, 'Monday', 'Breakfast')}></td>
+            <td id = "TuesdayBreakfast" onClick = {this._onClick.bind(this, 'Tuesday', 'Breakfast')}></td>
+            <td id = "WednesdayBreakfast" onClick = {this._onClick.bind(this, 'Wednesday', 'Breakfast')}></td>
+            <td id = "ThursdayBreakfast" onClick = {this._onClick.bind(this, 'Thursday', 'Breakfast')}></td>
+            <td id = "FridayBreakfast" onClick = {this._onClick.bind(this, 'Friday', 'Breakfast')}></td>
             <td id = "7" onClick = {this._onClick.bind(this, 'Saturday', 'Breakfast')}></td>
 
           </tr>
           <tr>
           <td>Lunch</td>
-          <td id = "8" onClick = {this._onClick.bind(this, 'Sunday', 'Lunch')}></td>
-          <td id = "9" onClick = {this._onClick.bind(this, 'Monday', 'Lunch')}></td>
-          <td id = "10" onClick = {this._onClick.bind(this, 'Tuesday', 'Lunch')}></td>
-          <td id = "11" onClick = {this._onClick.bind(this, 'Wednesday', 'Lunch')}></td>
-          <td id = "12" onClick = {this._onClick.bind(this, 'Thursday', 'Lunch')}></td>
-          <td id = "13" onClick = {this._onClick.bind(this, 'Friday', 'Lunch')}></td>
-          <td id = "14" onClick = {this._onClick.bind(this, 'Saturday', 'Lunch')}></td>
+          <td id = "SundayLunch" onClick = {this._onClick.bind(this, 'Sunday', 'Lunch')}></td>
+          <td id = "MondayLunch" onClick = {this._onClick.bind(this, 'Monday', 'Lunch')}></td>
+          <td id = "TuesdayLunch" onClick = {this._onClick.bind(this, 'Tuesday', 'Lunch')}></td>
+          <td id = "WednesdayLunch" onClick = {this._onClick.bind(this, 'Wednesday', 'Lunch')}></td>
+          <td id = "ThursdayLunch" onClick = {this._onClick.bind(this, 'Thursday', 'Lunch')}></td>
+          <td id = "FridayLunch" onClick = {this._onClick.bind(this, 'Friday', 'Lunch')}></td>
+          <td id = "SaturdayLunch" onClick = {this._onClick.bind(this, 'Saturday', 'Lunch')}></td>
           </tr>
 
           <tr>
           <td>Dinner </td>
-          <td id = "15" onClick = {this._onClick.bind(this, 'Sunday', 'Dinner')}></td>
-          <td id = "16" onClick = {this._onClick.bind(this, 'Monday', 'Dinner')}></td>
-          <td id = "17" onClick = {this._onClick.bind(this, 'Tuesday', 'Dinner')}></td>
-          <td id = "18" onClick = {this._onClick.bind(this, 'Wednesday', 'Dinner')}></td>
-          <td id = "19" onClick = {this._onClick.bind(this, 'Thursday', 'Dinner')}></td>
-          <td id = "20" onClick = {this._onClick.bind(this, 'Friday', 'Dinner')}></td>
-          <td id = "21" onClick = {this._onClick.bind(this, 'Saturday', 'Dinner')}></td>
+          <td id = "SundayDinner" onClick = {this._onClick.bind(this, 'Sunday', 'Dinner')}></td>
+          <td id = "MondayDinner" onClick = {this._onClick.bind(this, 'Monday', 'Dinner')}></td>
+          <td id = "TuesdayDinner" onClick = {this._onClick.bind(this, 'Tuesday', 'Dinner')}></td>
+          <td id = "WednesdayDinner" onClick = {this._onClick.bind(this, 'Wednesday', 'Dinner')}></td>
+          <td id = "ThursdayDinner" onClick = {this._onClick.bind(this, 'Thursday', 'Dinner')}></td>
+          <td id = "FridayDinner" onClick = {this._onClick.bind(this, 'Friday', 'Dinner')}></td>
+          <td id = "SaturdayDinner" onClick = {this._onClick.bind(this, 'Saturday', 'Dinner')}></td>
           </tr>
         </tbody>
       </table>
