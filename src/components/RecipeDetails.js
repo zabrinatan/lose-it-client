9b16513b2dc71cc9d71680520d43edf4d1e8fbbe
Zabrina L <zabrinatan@hotmail.com>
2018-08-28 14:21:06 +1000

    created a page with picture and label for show page of recipes

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -6,8 +6,13 @@ import Header from './Header'
 
 const SERVER_MY_RECIPES = 'http://localhost:3000/recipes.json'
 class RecipeDetails extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.state = {
+      recipe_id: this.props.match.params.id,
+      data: []
+    }
+
   }
 
 componentDidMount(){
@@ -19,7 +24,9 @@ fetchRecipeDetails(){
              "Authorization": localStorage.getItem('jwt')
            }
     }).then((response)=>{
-      console.log(response)
+        this.setState ({
+          data: response.data
+        })
 
       })
   }
@@ -30,6 +37,16 @@ fetchRecipeDetails(){
     return(
       <div>
       <h1>HEllo</h1>
+      {this.state.data.map((item)=> {
+          if(item.id == this.state.recipe_id){
+            return  <div>
+            <h2> {item.label}</h2>
+            <img src = {item.image} />
+            </div>
+          }
+
+      })}
+
       </div>
     )
   }
